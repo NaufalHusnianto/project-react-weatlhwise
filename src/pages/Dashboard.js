aec26f410877a2d85153bada1933b16e15914113
Naufal Husnianto <111817684+NaufalHusnianto@users.noreply.github.com>
2023-12-06 08:18:40 +0700

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -13,6 +13,10 @@ function Dashboard() {
         color: '#CD86C9'
     }
 
+    const purpleBgColor = {
+        backgroundColor: '#CD86C9'
+    }
+
     return (
         <div className="App p-4" style={bodyBg}>
             <header className="d-flex justify-content-sm-between align-items-center h-15 pt-2">
@@ -25,15 +29,16 @@ function Dashboard() {
                         <li className="nav-item"><a className="nav-link text-white" href="">Tentang WealthWise</a></li>
                         <li className="nav-item"><a className="nav-link text-white" href="">Bantuan</a></li>
                     </ul>
+                    <button type="button" className="btn btn-secondary btn-sm rounded-pill font-weight-bold text-dark pl-4 pr-4 ml-4" style={purpleBgColor}>Sign In</button>
                 </nav>
             </header>
             <main className="d-flex h-85">
-                <div className="container w-50 p-4 ml-5 mt-4">
+                <div className="container w-50 p-4 ml-5 mt-5">
                     <h1 className="card-title text-white font-style-dashboard">Inovasi <span className="font-weight-bold" style={purpleColor}>Finansial</span></h1>
                     <h1 className="card-title text-white font-style-dashboard">Untuk Masa Depan</h1>
                     <h1 className="card-title text-white font-style-dashboard">Yang Lebih <span className="font-weight-bold" style={purpleColor}>Cerah</span></h1>
-                    <p className="text-white mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis reprehenderit tenetur cupiditate magni suscipit in eaque assumenda soluta repellendus voluptate. Aspernatur corporis quas totam pariatur, sint quo tenetur excepturi consequatur.</p>
-                    <button type="button" className="btn btn-secondary btn-sm rounded-pill p-2 mt-2">Bergabung Sekarang!</button>
+                    <p className="text-white text-sm-left mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis reprehenderit tenetur cupiditate magni suscipit in eaque assumenda soluta repellendus voluptate. Aspernatur corporis quas totam pariatur, sint quo tenetur excepturi consequatur.</p>
+                    <button type="button" className="btn btn-secondary btn-sm rounded-pill font-weight-bold text-dark p-2 mt-2" style={purpleBgColor}>Bergabung Sekarang!</button>
                 </div>
                 <div className="container w-50">
                     <img src="/image/ball.png" className="position-absolute rotate-animation" style={{ width: '250px', height: 'auto', right: '270px', top: '130px'}}/>
