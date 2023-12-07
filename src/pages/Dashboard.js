c42bf015691fa2d902441366fc955c7db764690c
Naufal Husnianto <111817684+NaufalHusnianto@users.noreply.github.com>
2023-12-07 22:36:12 +0700

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -18,7 +18,7 @@ function Dashboard() {
     }
 
     return (
-        <div className="App p-4" style={bodyBg}>
+        <div className="App p-4 h-100vh" style={bodyBg}>
             <header className="d-flex justify-content-between align-items-center pt-2">
                 <h2 className="pl-5 text-white font-weight-normal text-sm-left">WealthWise <span style={purpleColor}>Finance</span></h2>
                 <nav className="navbar navbar-expand-lg bg-body-tertiary d-flex ">
@@ -32,17 +32,17 @@ function Dashboard() {
                     <button type="button" className="btn btn-secondary btn-sm rounded-pill font-weight-bold text-dark pl-4 pr-4 ml-4" style={purpleBgColor}>Sign In</button>
                 </nav>
             </header>
-            <main className="d-flex flex-wrap-reverse justify-content-center align-items-center h-85">
-                <div className="container w-100 col-lg-6 p-4 mt-5">
+            <main className="d-flex flex-wrap-reverse flex-row justify-content-center align-items-center h-85">
+                <div className="container w-100 h-100 col-lg-6 p-5 mt-2">
                     <h1 className="card-title text-white font-style-dashboard">Inovasi <span className="font-weight-bold" style={purpleColor}>Finansial</span></h1>
                     <h1 className="card-title text-white font-style-dashboard">Untuk Masa Depan</h1>
                     <h1 className="card-title text-white font-style-dashboard">Yang Lebih <span className="font-weight-bold" style={purpleColor}>Cerah</span></h1>
                     <p className="text-white text-sm-left mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis reprehenderit tenetur cupiditate magni suscipit in eaque assumenda soluta repellendus voluptate. Aspernatur corporis quas totam pariatur, sint quo tenetur excepturi consequatur.</p>
                     <button type="button" className="btn btn-secondary btn-sm rounded-pill font-weight-bold text-dark p-2 mt-2" style={purpleBgColor}>Bergabung Sekarang!</button>
                 </div>
-                <div className="container w-100 col-lg-6 text-center mb-5 position-relative">
-                    <img src="/image/ball.png" className="rotate-animation img-fluid" />
-                    <img src="/image/u.png" className="floating-animation img-fluid" />
+                <div className="container w-100 col-lg-6 text-center mb-5 position-relative d-flex align-items-center justify-content-center flex-column">
+                    <img src="/image/ball.png" className="rotate-animation img-fluid position-absolute" style={{ maxWidth: '12rem', bottom: '20%', right: '40%', transform: 'translate(-50%, -50%)' }} alt="Ball"/>
+                    <img src="/image/u.png" className="floating-animation img-fluid position-absolute" style={{ maxWidth: '28rem', transform: 'translate(-50%, -50%)' }} alt="U" />
                 </div>
             </main>
         </div>
